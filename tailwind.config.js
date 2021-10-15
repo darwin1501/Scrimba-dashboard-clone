51a4bc5ff35722a9b95385abf2a7bfb61aef9403
darwin1501 <darwin.pablo1501@gmail.com>
2021-10-15 21:24:32 +0800

    getting started

---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    'index.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      'xs': '320px',
+      'xs2': '476px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      colors:{
+        blue:{
+            paleBlue: '#e7f1ea'
+        },
+        indigo:{
+          scrimbaIndigo: '#5035ff',
+          scrimbaSkyblue: '#ddebf8'
+        },
+        orange:{
+          paleOrange: '#fffad1',
+          orangePumpkin: '#ffc06d'
+        },
+        pink:{
+          palePink: '#fbe5f3',
+          ligthPink: '#e7a4d0'
+        },
+        red:{
+          tomatoe: '#ff8080'
+        }
+      },
+      spacing:{
+        // 45%
+          '9/20': '45%',
+          '6/20': '30%'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
