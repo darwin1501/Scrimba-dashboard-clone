12dba7ada5c36740cd4a942d42a2aea1731acf03
darwin1501 <darwin.pablo1501@gmail.com>
2021-10-21 15:51:28 +0800

    add darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   purge: [
     'index.html'
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     screens: {
       'xs': '320px',
